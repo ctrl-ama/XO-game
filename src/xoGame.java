a5318361821df2af60b0954b998a38a94d9515c2
Amayindi <amayindi.yerima@binghamuni.edu.ng>
2023-10-23 11:13:10 -0700

    hi

---
+++ src/xoGame.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class xoGame {
+
+    private char[][] board;
+    private char currentPlayer;
+
+    public xoGame() {
+        board = new char[3][3];
+        currentPlayer = 'X';
+        initializeBoard();
+    }
+
+    public void initializeBoard (){
+        for (int i=0; i<2; i++) {
+            board[i][j] = ' ';
+        }
+    }
+
+
+}
