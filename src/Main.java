12b1a5921274bf18dea3681dee22680c28fd183a
bOLA-532 <faithadebolaibrahim@gmail.com>
2023-10-24 12:31:40 -0700

    ;)

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,15 @@
 public class Main {
+    public static void main(String[] args) {
+        xoGame game = new xoGame();
 
+        // Make moves for the players (you can add more moves as needed)
+        game.makeMove(0, 0); // Player X's move
+        game.makeMove(1, 1); // Player O's move
+        game.makeMove(0, 1); // Player X's move
+
+        // Get and print the current player
+        System.out.println("Current player: " + game.getCurrentPlayer());
+
+        // You can continue making moves and checking for a win or draw
+    }
 }
--- src/xoGame.java
+++ src/xoGame.java
@@ -18,10 +18,40 @@ public class xoGame {
             }
         }
     }
+    public boolean makeMove(int row, int col) {
+        if (row < 0 || row >= 3 || col < 0 || col >= 3 || board[row][col] != ' ') {
+            return false; // Invalid move
+        }
+        board[row][col] = currentPlayer;
+        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+        return true; // Valid move
+    }
+    public char checkWin() {
+        // Check rows, columns, and diagonals for a win
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' ') {
+                return board[i][0];
+            }
+            if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != ' ') {
+                return board[0][i];
+            }
+        }
+
+        if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ') {
+            return board[0][0];
+        }
+
+        if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ') {
+            return board[0][2];
+        }
+
+        return ' ';
+    }
+
 
     public char getCurrentPlayer() {
         return currentPlayer;
-    }
+    }}
 
 
 
