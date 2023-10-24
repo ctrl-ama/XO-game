4760524d10392bbc2c5b7bd9000bdf0a310f6312
bOLA-532 <faithadebolaibrahim@gmail.com>
2023-10-24 11:26:42 -0700

    ;)

--- src/xoGame.java
+++ src/xoGame.java
@@ -13,6 +13,7 @@ public class xoGame {
 
     public void initializeBoard (){
         for (int i=0; i<2; i++) {
+            for (int j = 0; j < 3; j++)
             board[i][j] = ' ';
         }
     }
