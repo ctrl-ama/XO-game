5015b4d5a69ac145fc7a71fe9ae824d4cf654253
Tyshe-81 <adeniyioluwatishe@gmail.com>
2023-10-24 11:45:04 -0700

    hiiiiii

--- src/xoGame.java
+++ src/xoGame.java
@@ -11,12 +11,23 @@ public class xoGame {
         initializeBoard();
     }
 
-    public void initializeBoard (){
-        for (int i=0; i<2; i++) {
-            for (int j = 0; j < 3; j++)
-            board[i][j] = ' ';
+    public void initializeBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = ' ';
+            }
         }
     }
 
+    public char getCurrentPlayer() {
+        return currentPlayer;
+    }
 
-}
+
+
+
+
+
+
+
+
